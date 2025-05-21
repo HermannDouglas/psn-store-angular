cc1e0c35e24c81205087ce940db6808c7a19dea4
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2025-05-21 12:49:41 -0500

    Feat(card): Tornar CardComponent dinâmico com dados via @Input
    - Adicionar propriedades @Input() para gameCover, gameLabel, gameType e gamePrice no CardComponent
    - Substituir valores estáticos do template por bindings dinâmicos
    - Corrigir fonte no CardPricing de 'Calibre' para 'Calibri'
    - Popular a página Home com quatro cards utilizando diferentes dados e imagens
    - Tornar o componente Card reutilizável e parametrizável para qualquer jogo

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: false,
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+  @Input()
+  gameCover: string = '';
+  @Input()
+  gameLabel: string = '';
+  @Input()
+  gameType: string = 'XPTO | PS4';
+  @Input()
+  gamePrice: string = 'R$ 399,90';
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
